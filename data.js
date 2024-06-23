72d148257cc6fcabba665b50d681108f0fdd01cb
Donwonmagic <165959419+Donwonmagic@users.noreply.github.com>
2024-06-23 17:05:45 -0400

    Image add

--- data.js
+++ data.js
@@ -1,18 +1,18 @@
 [
     {
-        "url": "https://www.atlanticrecords.com/sites/g/files/g2000015596/files/styles/artist_image_detail/public/2023-01/GUCCI%2022%20%282%29_0square.jpg?itok=bG3Emlyn",
+        "url": "Gucci-Mane.jpg",
         "alt": "Gucci Mane"
     },
     {
-        "url": "https://townsquare.media/site/812/files/2020/12/pooh-shiesty.jpg?w=780&q=75",
+        "url": "Pooh-Shiesty.jpeg",
         "alt": "Pooh Shiesty"
     },
     {
-        "url": "https://d33byq9npfy6u9.cloudfront.net/4/2023/02/20114714/secret-weapon-cover.jpg",
+        "url": "Scarr.jpg",
         "alt": "Big Scarr"
     },
     {
-        "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFa4qpYZh8KTnIQu8wWMVtWahoU9diRxOgNg&s",
+        "url": "Foogiano.jpg",
         "alt": "Foogiano"
     }
 ]
